306a0532863ae381598e71b27ccbb38c2e020d31
Alexandr Kobzev <sasha_kobzev18@mail.ru>
2026-02-09 19:29:33 +0300

    feature: add init migrations

---
+++ migrations/000001_init.down.sql
@@ -0,0 +1,2 @@
+DROP tracks IF EXISTS;
+DROP INDEX IF EXISTS tracks_created_at_idx;
---
+++ migrations/000001_init.up.sql
@@ -0,0 +1,13 @@
+CREATE TABLE tracks (
+    id BIGSERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+
+    origin_bucket TEXT NOT NULL,
+    origin_key TEXT NOT NULL,
+
+    hls_bucket TEXT,
+    hls_prefix TEXT
+);
+
+CREATE INDEX tracks_created_at_idx ON tracks(created_at DESC);
