f1a216255e7d7f922f502bcb4e03744ea79030ac
Alexandr Kobzev <sasha_kobzev18@mail.ru>
2026-02-18 00:47:25 +0300

    chore: define configuration structs for HTTP server, PostgreSQL, and MinIO client

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,11 +3,38 @@ package config
 import (
 	"log"
 	"os"
+	"time"
 
 	"github.com/ilyakaznacheev/cleanenv"
 )
 
 type Config struct {
+	Env         string      `yaml:"env"`
+	HTTPServer  HTTPServer  `yaml:"http_server"`
+	PostgreSQL  PostgreSQL  `yaml:"postgresql"`
+	MinIOClient MinIOClient `yaml:"minio_client"`
+}
+
+type HTTPServer struct {
+	Host        string        `yaml:"host"`
+	Port        int           `yaml:"port"`
+	Timeout     time.Duration `yaml:"timeout"`
+	IdleTimeout time.Duration `yaml:"idle_timeout"`
+}
+
+type PostgreSQL struct {
+	Host     string `yaml:"host"`
+	Port     int    `yaml:"port"`
+	DBName   string `yaml:"db_name"`
+	UserName string `yaml:"user_name"`
+	Password string `yaml:"password" env_required:"PGSQL_PASSWORD"`
+}
+
+type MinIOClient struct {
+	endpoint        string `yaml:"endpoint"`
+	accessKeyID     string `yaml:"access_key_id"`
+	secretAccessKey string `yaml:"secret_access_key" env_required:"MINIO_SECRET_ACCESS_KEY"`
+	useSSL          bool   `yaml:"use_ssl"`
 }
 
 func Load() *Config {
