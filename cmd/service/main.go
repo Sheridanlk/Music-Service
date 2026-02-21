bede1cbe8b939090d8728e11f5760086abe225c2
Alexandr Kobzev <sasha_kobzev18@mail.ru>
2026-02-21 20:36:44 +0300

    feature: enhance main function with graceful shutdown

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -1,6 +1,12 @@
 package main
 
 import (
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/Sheridanlk/Music-Service/internal/app"
 	"github.com/Sheridanlk/Music-Service/internal/config"
 	"github.com/Sheridanlk/Music-Service/internal/logger"
 )
@@ -8,6 +14,21 @@ import (
 func main() {
 	cfg := config.Load()
 	log := logger.SetupLogger(cfg.Env)
-	
+
+	log.Info("starting music service", slog.String("env", cfg.Env))
+
+	application := app.New(log, cfg.PostgreSQL, cfg.HTTPServer, cfg.MinIOClient, cfg.MinioStorage)
+
+	go application.Server.Start()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
+
+	<-stop
+
+	application.Server.Stop()
+	application.Storage.Close()
+
+	log.Info("application stopped")
 
 }
