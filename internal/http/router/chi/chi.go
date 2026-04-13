2fdd88b4aaed8322d5f3b35b2c9f86894e130fea
Alexandr Kobzev <sasha_kobzev18@mail.ru>
2026-04-13 23:47:48 +0300

    feat: add logger middleware for enhanced request logging

--- internal/http/router/chi/chi.go
+++ internal/http/router/chi/chi.go
@@ -8,11 +8,17 @@ import (
 	"github.com/Sheridanlk/Music-Service/internal/http/handlers/track/list"
 	"github.com/Sheridanlk/Music-Service/internal/http/handlers/track/stream"
 	"github.com/Sheridanlk/Music-Service/internal/http/handlers/track/upload"
+	"github.com/Sheridanlk/Music-Service/internal/http/middleware/logger"
 	chigo "github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func Setup(log *slog.Logger, trackUploader upload.TrackUploader, streamer stream.Streamer, lister list.Lister) http.Handler {
 	router := chigo.NewRouter()
+
+	router.Use(middleware.RequestID)
+	router.Use(logger.New(log))
+	router.Use(middleware.Recoverer)
 
 	router.Get("/player", player.New())
 
